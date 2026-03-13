8d55b9df6f183a41edf1099f12baadfeffaab7d4
nomyfo17 <nomyfo171234@gmail.com>
2026-03-12 21:05:22 -0700

    Update and rename ie4:x:waf-c.py to wafc.py

--- wafc.py
+++ wafc.py
@@ -1,4 +1,3 @@
-
 # "add2" add two numbers. like, add2(5, 6) will rusult "11"
 
 def add2(num1, num2):
@@ -10,6 +9,15 @@ def add2(num1, num2):
 def add3(num1, num2, num3):
     add3result = num1 + num2 + num3
     return add3result
+
+# "addall" adds any numbers. like, addall(5, 6, 1) will rusult "12"
+
+def addall(*args):
+    addallr = 0
+    for num in args:
+        addallr += num
+    return addallr
+
 
 # "multi2" multiply's two numbers. like, mult2(5, 6) will rusult "30"
 
@@ -23,6 +31,15 @@ def show(string1):
     showstring = string1
     print(showstring)
 
+# "showall" is to make a text show up on the users side. showall("message!", hi)
+
+def showall(*args):
+    string = ""
+    for s in args:
+        string += str(s) + " "
+    print(string.rstrip())
+    return string
+
 # "sub" subtract's two numbers. like, sub(5, 5) will rusult "1"
 
 def sub(num1, num2):
@@ -34,6 +51,8 @@ def sub(num1, num2):
 # "multi2" multiply's two numbers. like, mult2(5, 6) will rusult "30"
 # "add3" add three numbers. like, add3(5, 6, 1) will rusult "12"
 # "add2" add two numbers. like, add2(5, 6) will rusult "11"
+# "addall" adds any numbers. like, addall(5, 6, 1) will rusult "12"
+# "showall" is to make a text show up on the users side. showall("message!", hi)
 
 #if stuck try this as a base 
     #show("hello")
@@ -41,4 +60,5 @@ def sub(num1, num2):
 # start coding form this point, and on:
 
 #code HERE :
+
 
