925a6fb3c5c33ad60a6ea49de83e1a90223fd9b8
nomyfo17 <nomyfo171234@gmail.com>
2026-03-12 20:34:32 -0700

    waf-c v0.1
    beta's first release!

---
+++ ie4:x:waf-c.py
@@ -0,0 +1,44 @@
+
+# "add2" add two numbers. like, add2(5, 6) will rusult "11"
+
+def add2(num1, num2):
+    add2result = num1 + num2
+    return add2result
+
+# "add3" add three numbers. like, add3(5, 6, 1) will rusult "12"
+
+def add3(num1, num2, num3):
+    add3result = num1 + num2 + num3
+    return add3result
+
+# "multi2" multiply's two numbers. like, mult2(5, 6) will rusult "30"
+
+def multi2(num1, num2):
+    multi2result = num1 * num2
+    return multi2result
+
+# "show" is to make a text show up on the users side. show("message!")
+
+def show(string1):
+    showstring = string1
+    print(showstring)
+
+# "sub" subtract's two numbers. like, sub(5, 5) will rusult "1"
+
+def sub(num1, num2):
+    Subtracted = num1 - num2
+    return Subtracted
+
+# "sub" subtract's two numbers. like, sub(5, 5) will rusult "1"
+# "show" is to make a text show up on the users side. show("message!")
+# "multi2" multiply's two numbers. like, mult2(5, 6) will rusult "30"
+# "add3" add three numbers. like, add3(5, 6, 1) will rusult "12"
+# "add2" add two numbers. like, add2(5, 6) will rusult "11"
+
+#if stuck try this as a base 
+    #show("hello")
+
+# start coding form this point, and on:
+
+#code HERE :
+
